626114b8d7bd08ad5ba4d1d1ed878f04f2ecd0e9
Akash <thakare.akash007@gmail.com>
2024-11-11 14:16:02 +0530

    some currection

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,10 +19,10 @@ app.use(cookieParser());
 
 
 //routes import
-// import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js'
 
 //routes declarations
-// app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter)
 
 
 //http://localhost:8000/api/v1/users/register
