99369010c5497d27d88b852d919267788995abcb
Akash <thakare.akash007@gmail.com>
2024-11-16 10:33:47 +0530

    add Background component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Background from "./component/Background";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <div className="">
+    <Background>
+      
+    </Background>
+      </div>
     </>
   );
 }
---
+++ frontend/src/component/Background.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+const Background = () => {
+  return (
+    <div className="w-screen h-screen">
+      <Spline scene="https://prod.spline.design/1wQH2iVkI6Z4Iz1Q/scene.splinecode" />
+    </div>
+  );
+};
+
+export default Background;
