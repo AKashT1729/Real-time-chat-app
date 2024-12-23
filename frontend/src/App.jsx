ee8d89427c452d2fd32c0b791c7f5307be1e8158
Akash <thakare.akash007@gmail.com>
2024-12-23 15:48:27 +0530

    Verify user uesing  Outlet react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,21 @@ import Register from "./pages/Register";
 import Home from "./component/Home";
 import About from "./component/About";
 import ChatDashboard from "./component/ChatDashboard";
+import { VerifyUser } from "./utils/VerifyUser";
 
 function App() {
   return (
     <>
-
-       <Routes>
-        <Route index element= {<Home/>} />
-        <Route path="about" element= {<About/>}/>
-        <Route path="login" element={<LogIn/>}/>
-        <Route path="register" element={<Register/>}/>
-        <Route path="chatdashboard" element={<ChatDashboard/>}/>
-       </Routes>
-       </>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<LogIn />} />
+        <Route path="register" element={<Register />} />
+        <Route element={<VerifyUser />}>
+          <Route path="chatdashboard" element={<ChatDashboard />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/utils/VerifyUser.jsx
+++ frontend/src/utils/VerifyUser.jsx
@@ -1,3 +1,7 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext"
 
-
-
+export const VerifyUser = ()=>{
+    const {authUser} = useAuth();
+    return authUser ? <Outlet/> : <Navigate to ={'/login'}/>
+}
